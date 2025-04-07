f857306c72bc47161ae70071e29838909915b84d
leko-szabolcs <szabolcsworks@gmail.com>
2025-04-07 20:39:55 +0200

    UI_Version_5.0: Esemény hozzáadás Fragment UI részét elkészítettem + hozzáadtam a DatePicker-t!

--- app/src/main/java/beadando/isports_app/fragments/AddEventFragment.java
+++ app/src/main/java/beadando/isports_app/fragments/AddEventFragment.java
@@ -1,19 +1,119 @@
 package beadando.isports_app.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import beadando.isports_app.R;
 
 public class AddEventFragment extends Fragment {
+
+    private EditText etDate;
+    private Calendar calendar;
+
+    private Button btnUpload;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_event, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        calendar = Calendar.getInstance();
+
+        etDate = view.findViewById(R.id.etDate);
+
+        btnUpload = view.findViewById(R.id.btnUpload);
+
+        setupDatePicker();
+
+        btnUpload.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_addEventFragment_to_mainFragment);
+        });
+    }
+
+    private void setupDatePicker() {
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, monthOfYear);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                        showTimePickerDialog();
+                    }
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+
+        datePickerDialog.show();
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                requireContext(),
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        updateDateTimeField();
+                    }
+                },
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                true
+        );
+
+        timePickerDialog.show();
+    }
+
+    private void updateDateTimeField() {
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy. MM. dd. HH:mm", Locale.getDefault());
+        etDate.setText(dateTimeFormat.format(calendar.getTime()));
+    }
+
+    private void updateDateField() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy. MM. dd.", Locale.getDefault());
+        etDate.setText(dateFormat.format(calendar.getTime()));
     }
 }
