3b79465c6fd2a4d65609400d91c8458b5d2a9cac
kvilmos <kvilmos47@gmail.com>
2025-04-30 12:13:23 +0200

    Profil adatok megjelenítése, ProfilFragment egyszerűsítése

--- app/src/main/java/beadando/isports_app/fragments/editprofile/EditProfileFragment.java
+++ app/src/main/java/beadando/isports_app/fragments/editprofile/EditProfileFragment.java
@@ -1,4 +1,4 @@
-package beadando.isports_app.fragments;
+package beadando.isports_app.fragments.editprofile;
 
 import android.os.Bundle;
 
@@ -12,8 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import java.util.Calendar;
 
 import beadando.isports_app.R;
 
